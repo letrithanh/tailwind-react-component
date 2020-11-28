de722f3bbd384ef394a98b7b0012ab1523c2d641
Thanh Le Tri <letrithanh@outlook.com>
2020-11-28 15:53:22 +0700

    move header example to another page

---
+++ pages/header.tsx
@@ -0,0 +1,26 @@
+import { IHeaderItem } from "../components/header/header-item/header-item.interface";
+import Header from "../components/header/header.component";
+
+const HeaderComponentPage = () => {
+
+    const headerItems: IHeaderItem[] = [];
+    const selectedHeaderItem: IHeaderItem = {id: 1, value: "Dashboard", path: "#"};
+    headerItems.push(selectedHeaderItem);
+    headerItems.push({id: 2, value: "Team", path: "#"});
+    headerItems.push({id: 3, value: "Projects", path: "#"});
+    headerItems.push({id: 4, value: "Calendar", path: "#"});
+    headerItems.push({id: 5, value: "Reports", path: "#"});
+
+
+    return (
+        <>
+            <Header logoSVGSrc="/svg/unicorn.svg" items={headerItems} selectedItem={selectedHeaderItem}/>
+
+            <div>
+                <div>Icons made by <a href="https://www.flaticon.com">Flaticon</a></div>
+            </div>
+        </>
+    );
+}
+
+export default HeaderComponentPage;
